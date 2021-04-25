e812c58afb07651fac0539b0c36a7be40bdedde5
Pavlo <71569680+p4v10@users.noreply.github.com>
2021-04-25 12:39:50 -0500

    response

--- blockchain.py
+++ blockchain.py
@@ -84,4 +84,8 @@ def mine_block():
     return jsonify(response), 200
 
 # Getting the full Blockchain
-
+@app.route('/get_chain', method = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
