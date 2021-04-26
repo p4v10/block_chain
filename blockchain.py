e34eb0b05f8b624e774fe2b222bf49e8e0d9eeaa
Pavlo <71569680+p4v10@users.noreply.github.com>
2021-04-26 09:37:05 -0500

    upd

--- blockchain.py
+++ blockchain.py
@@ -48,7 +48,7 @@ class Blockchain:
         block_index = 1
         while block_index < len(chain):
             block = chain[block_index]
-            if block['previous_hash'] != self.hash[previous_block]:
+            if block['previous_hash'] != self.hash(previous_block):
                 return False
             previous_proof = previous_block['proof']
             proof = block['proof']
@@ -64,6 +64,7 @@ class Blockchain:
         
 # Creating a Web App
 app = Flask(__name__)
+
 
 # Creating Blockchain
 blockchain = Blockchain()
@@ -88,6 +89,16 @@ def mine_block():
 def get_chain():
     response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+# Checking if the Blockchain is valid
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid.'}
+    else:
+        response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
     return jsonify(response), 200
 
 # Running the app
